942b1d3a3aeb9c7a53474f6ad7bebb92acabee1b
Johan Durish <jdurish2@gmail.com>
2025-05-07 10:51:33 -0400

    Add location form

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import { weatherInLocation } from "./modules/weather";
+
+const LOCATION_FORM_SUBMIT_MILLISECOND_DELAY = 1500;
+
+const locationForm = document.querySelector("#location");
+
+let locationFormSubmitTimeout;
+
+locationForm.addEventListener("input", (event) => {
+   clearTimeout(locationFormSubmitTimeout);
+
+   locationFormSubmitTimeout = setTimeout(() => {
+      locationForm.requestSubmit();
+   }, LOCATION_FORM_SUBMIT_MILLISECOND_DELAY);
+});
+
+locationForm.addEventListener("submit", (event) => {
+   event.preventDefault();
+
+   clearTimeout(locationFormSubmitTimeout);
+
+   const locationFormData = new FormData(locationForm);
+   
+   const location = locationFormData.get("location");
+   
+   if (!location) {
+      return;
+   }
+
+   const locationFormFieldset = locationForm.querySelector("fieldset");
+   
+   locationFormFieldset.disabled = true;
+         
+   weatherInLocation(location)
+   .then((result) => {
+      console.log(result.resolvedAddress);
+      console.log(result.description);
+   })
+   .catch((error) => {
+      console.log(error);
+   })
+   .finally(() => {
+      locationFormFieldset.disabled = false;
+   });
+});
